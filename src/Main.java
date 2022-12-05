9ba3e4d4764737e835d9121b3bbdb6880181954d
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-05 22:04:58 +0300

    задание 1 выполнено

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        System.out.println("задание 1");
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Если возраст человека " + age + " лет, он совершеннолетний");
+        }
+        int age2 = age - 10;
+        if (age2 < 18) {
+            System.out.println("Если возраст человека " + age2 + " лет, он не совершеннолетний");
+        }
+        //task2
+        System.out.println("задание 2");
+
+    }
+}
