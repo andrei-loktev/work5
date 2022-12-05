9160061667e1d078e85b8859d22dd6bd11607de9
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-05 23:09:04 +0300

    задание 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         if (age >= 18) {
             System.out.println("Если возраст человека " + age + " лет, он совершеннолетний");
         }
-        int age2 = age - 10;
+        int age2 = 15;
         if (age2 < 18) {
             System.out.println("Если возраст человека " + age2 + " лет, он не совершеннолетний");
         }
@@ -16,12 +16,19 @@ public class Main {
         if (temperatureUnit < 5) {
             System.out.println("На улице " + temperatureUnit + " градуса, нужно надеть шапку");
         }
-        int temperatureUnit2 = temperatureUnit + 4;
+        int temperatureUnit2 = 7;
         if (temperatureUnit2 >= 5) {
             System.out.println("На улице " + temperatureUnit2 + " градусов, можно идти без шапки");
         }
         //task3
         System.out.println("задание 3");
-
+        int speedDriver = 60;
+        if (speedDriver <= 60) {
+            System.out.println("Если скорость водителя " + speedDriver + " ,штрафа нет");
+        int speedDriver2 = 80;
+        if (speedDriver2 > 60) {
+            System.out.println("Если скорость водителя " + speedDriver2 + " ,штраф есть");
+        }
+        }
     }
 }
